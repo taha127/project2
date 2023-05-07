5fd398fa51436be8daf797718c33d20a84e28e00
taha <tahadehghani127@gmail.com>
2023-05-07 23:58:16 +0330

    Modify removeByName function than overload the (-) operator and modify other operators

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include <windows.h>
 class ContactNode {
 private:
     std::string name;
@@ -48,7 +49,7 @@ ContactNode::ContactNode(const std::string& name = "", const std::string& email 
 class ContactsList {
 private:
     ContactNode* head;
-    ContactNode* title;
+    ContactNode* tail;
     int size;
 public:
     
@@ -89,24 +90,24 @@ public:
 // Constructors for Contactslist
 ContactsList::ContactsList(){
     head = 0;
-    title = 0;
+    tail = 0;
     size = 0;
 }
 ContactsList::ContactsList(const ContactsList& other){
     head = 0;
-    title = 0;
+    tail = 0;
     size = 0;
     ContactNode * temp = other.head;
     while(temp){
-        push_back(temp -> getName(), temp -> getEmail(), temp -> getPhone());
-        temp = temp -> getNext();
+        push_back(temp->getName(), temp->getEmail(), temp->getPhone());
+        temp = temp->getNext();
     } 
 }
 // Destructor for Contactslist
 ContactsList::~ContactsList(){
     ContactNode* temp = head;
     while(head){
-        head = head -> getNext();
+        head = head->getNext();
         delete temp;
         temp = head;
     }
@@ -114,13 +115,13 @@ ContactsList::~ContactsList(){
 void ContactsList::push_front(const std::string& name, const std::string& email,
         const std::string& phone){
             ContactNode * temp = new ContactNode;
-            temp -> setName(name);
-            temp -> setEmail(email);
-            temp -> setPhone(phone);
-            temp -> setNext(head);
+            temp->setName(name);
+            temp->setEmail(email);
+            temp->setPhone(phone);
+            temp->setNext(head);
             head = temp;
             size++;
-            if(title == 0) title = head;
+            if(tail == 0) tail = head;
         }
 void ContactsList::push_back(const std::string& name, const std::string& email,
         const std::string& phone){
@@ -129,64 +130,82 @@ void ContactsList::push_back(const std::string& name, const std::string& email,
                 return;
             }
             ContactNode* temp = new ContactNode;
-            temp -> setName(name);
-            temp -> setEmail(email);
-            temp -> setPhone(phone);
-            temp -> setNext(0);
-            title -> setNext(temp);
-            title = temp;
+            temp->setName(name);
+            temp->setEmail(email);
+            temp->setPhone(phone);
+            temp->setNext(0);
+            tail->setNext(temp);
+            tail = temp;
             size++;
         }
 //operators
 ContactsList& ContactsList::operator=(const ContactsList& other){
-    this -> ~ContactsList();
+    this->~ContactsList();
     ContactNode * temp = other.head;
     while(temp){
-        this -> push_back(temp -> getName(), temp -> getEmail(), temp -> getPhone());
-        temp = temp -> getNext();
+        push_back(temp->getName(), temp->getEmail(), temp->getPhone());
+        temp = temp->getNext();
     }
     return *this;
 }
 ContactNode& ContactsList::operator[](int index){
     ContactNode * temp = head; 
     while(index){
-        temp = temp -> getNext();
+        temp = temp->getNext();
         index --;
     }
     return *temp;
 }
 bool ContactsList::operator==(const ContactsList& other){
-    ContactNode * temp = other.head;
-    ContactNode * self = this -> head;
-    if(this->size != other.size) return false;
-    bool answer = false;
-    while(temp){
-        while(self){
-            if(temp->getName() == self->getName() && temp->getEmail() == self->getEmail() && temp->getPhone() == self->getPhone())
-            {
-                answer = true;
-                break;
-            }
-            answer = false;
-            self = self->getNext();
+    ContactsList temp = other;
+    ContactsList self = *this;
+    if(size != other.size) return false;
+    if((temp - self).size == 0) return true;
+    return false;
+}
+ContactsList ContactsList::operator+(const ContactsList& other){
+    ContactsList answer = other;
+    for(int i = 0; i < size; i++){
+        int j;
+        for(j = 0; j < answer.size; j++){
+            if((*this)[i].getName() == answer[j].getName() && (*this)[i].getEmail() == answer[j].getEmail() && 
+                    (*this)[i].getPhone() == answer[j].getPhone()) break;
         }
-        temp = temp->getNext();
+        if(j == answer.size) 
+            answer.addContact((*this)[i].getName(), (*this)[i].getEmail(), (*this)[i].getPhone());
     }
     return answer;
 }
+ContactsList ContactsList::operator-(const ContactsList other){
+    ContactsList answer;
+    ContactsList temp = other;
+    for(int i = 0; i < size; i++){
+        ContactNode *exist_name = temp.searchByName((*this)[i].getName()), 
+        *exist_email = temp.searchByEmail((*this)[i].getEmail()),
+        *exist_phone = temp.searchByPhone((*this)[i].getPhone());
+        if(exist_name == 0 || exist_email == 0 || exist_phone == 0 || 
+            (exist_name != exist_email && exist_email != exist_phone)){
+                answer.addContact((*this)[i].getName(), (*this)[i].getEmail(), (*this)[i].getPhone());
+            }
+    }
+    return answer;
+}
+
+
 //methods
+
 // Insertion method
 void ContactsList::addContact(const std::string& name, const std::string& email,
         const std::string& phone){            
             for(int i = 0; i < size; i++){
-                if(name.compare(this->operator[](i).getName()) < 1){
+                if(name.compare((*this)[i].getName()) < 1){
                     if(i > 0){
                         ContactNode *temp = new ContactNode;
                         temp->setName(name);
                         temp->setEmail(email);
                         temp->setPhone(phone);
-                        temp->setNext(&this->operator[](i));
-                        this->operator[](i - 1).setNext(temp);
+                        temp->setNext(&(*this)[i]);
+                        (*this)[i - 1].setNext(temp);
                         size++;
                         return;
                     }
@@ -197,41 +216,76 @@ void ContactsList::addContact(const std::string& name, const std::string& email,
             push_back(name, email, phone);
         }
 // Deletion methods
-void ContactsList::removeByName(const std::string& name){// Removes the contact with matching name
-    for(int i = 0; i < size; i++){
-        if(this->operator[](i).getName() == name){
-            ContactNode * temp = &this->operator[](i);
-            size --;
-            if(i > 0){                 
-                this->operator[](i - 1).setNext(temp->getNext());
-                if(i == size - 1) title = &this->operator[](i - 1);
-                delete temp;                
-                return;
-            }
-            head = head->getNext();
-            delete temp;
+// Removes the contact with matching name
+void ContactsList::removeByName(const std::string& name){
+    std::vector<int>index;
+    int target;
+    for(int i = 0; i < size; i++)
+        if((*this)[i].getName() == name)
+            index.push_back(i);
+    if(index.size() == 0){
+        system("cls");
+        std::cout << "No such name found!\n";
+        return ;
+    }
+    if(index.size() == 1)
+        target = 1;
+        
+    else{
+        system("cls");
+        for(int i = 0; i < index.size(); i++){
+            std::cout << i + 1 << " >\n";
+            std::cout << "||###########################################||\n";
+            std::cout << "||name  ||  " << (*this)[index[i]].getName();
+            for(int j = 0; j < 33 - (*this)[index[i]].getName().size(); j++) std::cout << " ";
+            std::cout <<"||\n";
+            std::cout << "||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||\n";
+            std::cout << "||email ||  " << (*this)[index[i]].getEmail();
+            for(int j = 0; j < 33 - (*this)[index[i]].getEmail().size(); j++) std::cout << " ";
+            std::cout <<"||\n";
+            std::cout << "||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||\n";
+            std::cout << "||Phone ||  " << (*this)[index[i]].getPhone();
+            for(int j = 0; j < 33 - (*this)[index[i]].getPhone().size(); j++) std::cout << " ";
+            std::cout <<"||\n";
+            std::cout << "||###########################################||\n";
+        }
+        while(true){
+            std::cout << "which Contact do you want to delete?\n Enter its index : ";
+            std::cin >> target;
+            if(target < index.size() + 1 && target > 0) break;
+            std::cout << "incorrect value!\n";
         }
     }
+    ContactNode * temp = &(*this)[index[target - 1]];
+    size --;
+    if(index[target - 1] > 0){                 
+        (*this)[index[target - 1] - 1].setNext(temp->getNext());
+        if(index[target - 1] == size - 1) tail = &(*this)[index[target - 1] - 1];
+        delete temp;                
+        return;
+    }
+    head = head->getNext();
+    delete temp;
 } 
 // Search methods
 ContactNode* ContactsList::searchByName(const std::string& name){// Returns the node with matching name
     for(int i = 0; i < size; i++){
-        if(name == this->operator[](i).getName())
-            return &this->operator[](i);
+        if(name == (*this)[i].getName())
+            return &(*this)[i];
     }
     return 0;
 }    
 ContactNode* ContactsList::searchByEmail(const std::string& email){// Returns the node with matching email
     for(int i = 0; i < size; i++){
-        if(email == this->operator[](i).getEmail())
-            return &this->operator[](i);
+        if(email == (*this)[i].getEmail())
+            return &(*this)[i];
     }
     return 0;   
 }
 ContactNode* ContactsList::searchByPhone(const std::string& phone){// Returns the node with matching phone
     for(int i = 0; i < size; i++){
-        if(phone == this->operator[](i).getPhone())
-            return &this->operator[](i);
+        if(phone == (*this)[i].getPhone())
+            return &(*this)[i];
     }
     return 0;   
 }
@@ -245,44 +299,35 @@ void ContactsList::editContact(const std::string& name, const std::string& newEm
 void ContactsList::print(){
     std::cout << "||###########################################||\n";
     for(int i = 0; i < size; i++){
-        std::cout << "||name  ||  " << this->operator[](i).getName();
-        for(int j = 0; j < 33 - this->operator[](i).getName().size(); j++) std::cout << " ";
+        std::cout << "||name  ||  " << (*this)[i].getName();
+        for(int j = 0; j < 33 - (*this)[i].getName().size(); j++) std::cout << " ";
         std::cout <<"||\n";
         std::cout << "||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||\n";
-        std::cout << "||email ||  " << this->operator[](i).getEmail();
-        for(int j = 0; j < 33 - this->operator[](i).getEmail().size(); j++) std::cout << " ";
+        std::cout << "||email ||  " << (*this)[i].getEmail();
+        for(int j = 0; j < 33 - (*this)[i].getEmail().size(); j++) std::cout << " ";
         std::cout <<"||\n";
         std::cout << "||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~||\n";
-        std::cout << "||Phone ||  " << this->operator[](i).getPhone();
-        for(int j = 0; j < 33 - this->operator[](i).getPhone().size(); j++) std::cout << " ";
+        std::cout << "||Phone ||  " << (*this)[i].getPhone();
+        for(int j = 0; j < 33 - (*this)[i].getPhone().size(); j++) std::cout << " ";
         std::cout <<"||\n";
         std::cout << "||###########################################||\n";
     }
 }
 
-
-/*void ContactsList::print(){
-    ContactNode * temp = head;
-    std::cout << size << std::endl << std::endl;
-    while(temp){
-        std::cout << temp -> getName() << std::endl;
-        std::cout << temp -> getEmail() << std::endl;
-        std::cout << temp -> getPhone() << std::endl;
-        std::cout<< "--------------------------------------------------\n";
-        temp = temp -> getNext();
-    }
-}*/
-// main function to use all the methods
 int main(){
-    ContactsList x, z;
-    x.addContact("mohammad taha dehgani", "tahadehghani127@gmail.com", "35292764");
+    ContactsList x, z, y;
+    x.addContact("taha", "tahadehghani127@gmail.com", "35292764");
     x.addContact("ali", "email", "74573");
+    x.addContact("ali", "gmail", "7498495");
     x.addContact("hamed", "yaho", "35465");
-    x.addContact("javad", "apple", "3155");
-    x.addContact("ahmad", "ita", "4995");
-    x.removeByName("taha");
-    x.editContact("ali", "srosh", "0000");
-    x.editContact("ahmad", "aiphone", "232323");
-    x.editContact("taha", "iui", "38974");
-    x.print();
+    x.addContact("hamed", "apple", "3155");
+    x.addContact("ali", "you tube", "3784");
+
+    z.addContact("ali", "email", "74573");
+    z.addContact("javad", "apple", "3155");
+    bool t = x == x;
+    std::cout << t << std::endl;
+    t = x == z;
+    std::cout << t << std::endl;
+    
 }
